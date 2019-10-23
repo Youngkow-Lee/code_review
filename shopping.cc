74412f05cb0aa12723e137703e29a3ece2af6253
Youngkow-Lee <56816438+Youngkow-Lee@users.noreply.github.com>
2019-10-23 15:12:56 +0900

    불필요한 변수 삭제 및 for 문 가독성 향상
    std::vector<Goods>::iterator p = shopping_list_.begin(); -> 삭제
    for (std::vector<Goods>::iterator i = shopping_list_.begin(); i != shopping_list_.end()-1; i++) { // 80자 이내 코드 가독성 강화

--- shopping.cc
+++ shopping.cc
@@ -1,17 +1,14 @@
 #include <iostream>
 #include "./shopping.h"
 
-// Push Goods object to shopping_list_ vector
 void Shopping::PushList(Goods object) {
   shopping_list_.push_back(object);
 }
 
-// Get Goods object from end of shopping_list_ vector
 Goods Shopping::GetLastElement() {
   return shopping_list_.back();
 }
 
-// Print shopping_list_ Elements
 void Shopping::PrintList() {
   for (std::vector<Goods>::iterator i = shopping_list_.begin();
       i != shopping_list_.end(); i++) {
@@ -23,10 +20,9 @@ void Shopping::PrintList() {
 // Sort shopping_list_ Elements by Price
 // Apply Selection Sort algorithm
 void Shopping::SortByPrice() {
-  std::vector<Goods>::iterator p = shopping_list_.begin();
   std::vector<Goods>::iterator min = shopping_list_.end();
-  for (std::vector<Goods>::iterator i = shopping_list_.begin();
-      i != shopping_list_.end()-1; i++) {
+  
+  for (std::vector<Goods>::iterator i = shopping_list_.begin(); i != shopping_list_.end()-1; i++) {
     for (std::vector<Goods>::iterator j = i; j != shopping_list_.end(); j++) {
       if (min == shopping_list_.end()) {
         min = j;
@@ -44,10 +40,9 @@ void Shopping::SortByPrice() {
 // Sort shopping_list_ Elements by Index
 // Apply Selection Sort algorithm
 void Shopping::SortByIndex() {
-  std::vector<Goods>::iterator p = shopping_list_.begin();
   std::vector<Goods>::iterator min = shopping_list_.end();
-  for (std::vector<Goods>::iterator i = shopping_list_.begin();
-      i != shopping_list_.end()-1; i++) {
+  
+  for (std::vector<Goods>::iterator i = shopping_list_.begin(); i != shopping_list_.end()-1; i++) {
     for (std::vector<Goods>::iterator j = i; j != shopping_list_.end(); j++) {
       if (min == shopping_list_.end()) {
         min = j;
