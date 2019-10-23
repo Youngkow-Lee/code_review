b7143cf22c59bef4f93866f47177dd77ba313da7
Youngkow-Lee <56816438+Youngkow-Lee@users.noreply.github.com>
2019-10-23 16:13:15 +0900

    Shopping에 생성자 소멸자 추가
    Shopping에 생성자 소멸자 추가

--- shopping.h
+++ shopping.h
@@ -17,6 +17,10 @@
 
 class Shopping{
  public:
+  Shopping(std::vector<Goods> goods_list) {
+			 shopping_list_ = goods_list;
+  }
+  ~
   void PushList(Goods object);
   Goods GetLastElement();
   void PrintList();
