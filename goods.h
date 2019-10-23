ca3970c7cf82fff56f654adb6c20d0d22bd90050
Youngkow-Lee <56816438+Youngkow-Lee@users.noreply.github.com>
2019-10-23 15:05:46 +0900

    define guard 수정 및 class 초기화 코드 추가
    #define guard의 naming은 H
    
    초기값이 항상 필요로 해 보입니다.
    해당하는 함수 대신에 constructor를 활용하면 초기값 설정을 강제화 할 수 있어 보입니다.

--- goods.h
+++ goods.h
@@ -1,21 +1,27 @@
-#ifndef GOODS_H_
-#define GOODS_H_
+#ifndef CODEREVIEW_GOODS_H_
+#define CODEREVIEW_GOODS_H_
 
 // Goods class for representing goods information with index and price
 // Example:
 //    Goods test;
 //    test.SetGoodsInfo(1, 10000);
 //    std::cout << test.GetIndex() << " - " << test.GetPrice << endl;
+
 class Goods{
  public:
+  Goods(){
+      goods_index_ = 0;
+      goods_price_ = 0;
+  }
+  ~Goods();
   void SetGoodsInfo(int index, int price);
   int GetIndex();
   int GetPrice();
-
+ 
  private:
-  int goods_index_ = 0;
-  int goods_price_ = 0;
+  int goods_index_;
+  int goods_price_;
 };
 
 
-#endif  // GOODS_H_
+#endif  // CODEREVIEW_GOODS_H_
